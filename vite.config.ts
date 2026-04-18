dfeddfa614a47b9428aabfe3ce704723ac091f65
Ravindra Chauhan <w3ravindrachauhan@gmail.com>
2026-04-18 23:17:09 +0530

    fix: update base path in Vite configuration to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/infinite-quiz-app/',
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
